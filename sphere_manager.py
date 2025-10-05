3465c341ff3ed202fc84c96b8df85ef55a14553c
Randy Cheng <150641663+RafaelBinks@users.noreply.github.com>
2025-10-05 00:19:18 -0700

    Objects not all together

--- sphere_manager.py
+++ sphere_manager.py
@@ -10,6 +10,10 @@ class SphereManager:
         self.left_is_wall = random.choice([True, False])
         self.middle_is_wall = random.choice([True, False])
         self.right_is_wall = random.choice([True, False])
+        # Wait counters for each object
+        self.left_wait = random.randint(0, 60)
+        self.middle_wait = random.randint(0, 60)
+        self.right_wait = random.randint(0, 60)
         # Initial y positions for the spheres
         self.left_sphere_y = 0.0 if self.left_is_wall else random.choice([0.0, -3.0])
         self.middle_sphere_y = 0.0 if self.middle_is_wall else random.choice([0.0, -3.0])
@@ -25,17 +29,38 @@ class SphereManager:
     def reset_if_needed(self):
         # Reset spheres when they get too close
         if self.left_sphere_z > -5.0:
-            self.left_sphere_z = -50.0
-            self.left_is_wall = random.choice([True, False])
-            self.left_sphere_y = 0.0 if self.left_is_wall else random.choice([0.0, -3.0])
+            if self.left_wait > 0:
+                self.left_wait -= 1
+            else:
+                self.left_sphere_z = -50.0
+                self.left_is_wall = random.choice([True, False])
+                # If both others are walls, force this to be a sphere
+                if self.left_is_wall and self.middle_is_wall and self.right_is_wall:
+                    self.left_is_wall = False
+                self.left_sphere_y = 0.0 if self.left_is_wall else random.choice([0.0, -3.0])
+                self.left_wait = random.randint(0, 60)
         if self.middle_sphere_z > -5.0:
-            self.middle_sphere_z = -50.0
-            self.middle_is_wall = random.choice([True, False])
-            self.middle_sphere_y = 0.0 if self.middle_is_wall else random.choice([0.0, -3.0])
+            if self.middle_wait > 0:
+                self.middle_wait -= 1
+            else:
+                self.middle_sphere_z = -50.0
+                self.middle_is_wall = random.choice([True, False])
+                # If both others are walls, force this to be a sphere
+                if self.middle_is_wall and self.left_is_wall and self.right_is_wall:
+                    self.middle_is_wall = False
+                self.middle_sphere_y = 0.0 if self.middle_is_wall else random.choice([0.0, -3.0])
+                self.middle_wait = random.randint(0, 60)
         if self.right_sphere_z > -5.0:
-            self.right_sphere_z = -50.0
-            self.right_is_wall = random.choice([True, False])
-            self.right_sphere_y = 0.0 if self.right_is_wall else random.choice([0.0, -3.0])
+            if self.right_wait > 0:
+                self.right_wait -= 1
+            else:
+                self.right_sphere_z = -50.0
+                self.right_is_wall = random.choice([True, False])
+                # If both others are walls, force this to be a sphere
+                if self.right_is_wall and self.left_is_wall and self.middle_is_wall:
+                    self.right_is_wall = False
+                self.right_sphere_y = 0.0 if self.right_is_wall else random.choice([0.0, -3.0])
+                self.right_wait = random.randint(0, 60)
 
     def draw_objects(self, shapes, rotation_angle):
         # Draw left object
